4a84f2d0feb1128c4ec4a1bcac230f22ef8b4595
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-04 03:20:50 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,9 +1,7 @@
 # Your code here
 def meal_choice (veg1, veg2, ptotein)
-  if meal_choice (veg1, veg2, ptotein)
-puts "A plate of #{protein} with #{veg1} and #{veg2}"
-else meal_choice (veg1, veg2, ptotein ="tofu")
-  puts "A plate of #{protein} with #{veg1} and #{veg2}"
-end
-  puts "What a nutricious meal!"
-end
+   puts "What a nutricious meal!"
+ end 
+   puts "A plate of #{protein} with #{veg1} and #{veg2}"
+ end
+
